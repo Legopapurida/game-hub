2260836bb89747083d66d00272e6c378f937377d
Lego developer <papuridalego@gmail.com>
2024-07-16 22:25:21 +0330

    [IMP] Fetch genre

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card>
+    <Card width={{ lg: 350, base: "auto" }}>
       <Skeleton height={"200px"} />
       <CardBody>
         <SkeletonText />
