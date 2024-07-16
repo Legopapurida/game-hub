b034fadc56c96a6faaaa11589b04d853dd1d89f4
Lego developer <papuridalego@gmail.com>
2024-07-16 12:15:33 +0330

    [IMP] Displaying platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,40 @@
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { BsGlobe, BsNintendoSwitch } from "react-icons/bs";
+import {
+  FaAndroid,
+  FaApple,
+  FaLinux,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { Platform } from "../hooks/useGames";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: BsNintendoSwitch,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    mac: FaApple,
+    web: BsGlobe,
+    linux: FaLinux,
+  };
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color={"gray.500"} />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
