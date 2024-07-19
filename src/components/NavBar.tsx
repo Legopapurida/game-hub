fed1120652fd04f03794796ee021b7c46ef8e502
Lego developer <papuridalego@gmail.com>
2024-07-19 22:33:42 +0330

    [IMP] Build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,16 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 const NavBar = () => {
   return (
     <HStack
       padding={"10px"}
-      justifyContent={"space-between"}
+      // justifyContent={"space-between"}
       flexDirection={"row"}
     >
       <Image src={logo} boxSize="60px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
