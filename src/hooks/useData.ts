57d405cdddb3602214d9be472d6bc218614f14e7
Lego developer <papuridalego@gmail.com>
2024-08-02 22:23:00 +0330

    [IMP] export fetchresponse

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -2,7 +2,7 @@ import { AxiosRequestConfig, isCancel } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
-interface FetchResponse<T> {
+export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
