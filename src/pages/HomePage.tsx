3371e650ffb774f4a316364ea5fd42540a778953
Lego developer <papuridalego@gmail.com>
2024-08-18 19:09:59 +0330

    [ADD] pages

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,41 @@
+import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
+import GenreList from "../components/GenreList";
+import GameHeading from "../components/GameHeading";
+import PlatformSelector from "../components/PlatformSelector";
+import SortSelector from "../components/SortSelector";
+import GameGrid from "../components/GameGrid";
+
+const HomePage = () => {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"main"`,
+        lg: `"aside main"`, // 1024px
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+    >
+      <Show above="lg">
+        <GridItem area={"aside"} paddingLeft={"10px"}>
+          <GenreList />
+        </GridItem>
+      </Show>
+      <GridItem area={"main"}>
+        <Box paddingLeft={"10px"}>
+          <GameHeading />
+          <Flex marginBottom={5}>
+            <Box marginRight={5}>
+              <PlatformSelector />
+            </Box>
+            <SortSelector />
+          </Flex>
+        </Box>
+        <GameGrid />
+      </GridItem>
+    </Grid>
+  );
+};
+
+export default HomePage;
